2da1322cb807b278ef43cc99c63d88f7651536ca
peter <iampeter@home.com>
2021-08-20 13:54:31 +0900

    r

--- ydERP/src/main/java/com/yedam/erp/view/HomeController.java
+++ ydERP/src/main/java/com/yedam/erp/view/HomeController.java
@@ -19,9 +19,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.yedam.erp.stocks.LookupCodesVO;
 import com.yedam.erp.stocks.impl.HomeServiceImpl;
 
-/**
- * Handles requests for the application home page.
- */
 @Controller
 public class HomeController {
 
@@ -30,9 +27,6 @@ public class HomeController {
 
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
